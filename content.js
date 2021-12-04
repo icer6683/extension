5c06145fec299f491023b36bc5a863936e490daf
Andrew Lou <ayl72@cornell.edu>
2021-12-04 11:22:31 -0500

    Refined Timer Display

--- content.js
+++ content.js
@@ -1,6 +1,15 @@
 setInterval(
   function () {
     var date = new Date();
-    document.getElementById("clock").innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-  }, 1000)
+    semi = "AM";
+    hour = date.getHours();
+    minute = date.getMinutes();
+    second = date.getSeconds();
+    if (hour > 11) { semi = "PM"; }
+    if (hour > 12) { hour = hour - 12; }
+    if (hour == 0) { hour = 12; }
+    if (minute < 10) { minute = "0" + minute; }
+    if (second < 10) { second = "0" + second; }
+    document.getElementById("clock").innerHTML = hour + ":" + minute + ":" + second + " " + semi;
+  }, 500)
 
