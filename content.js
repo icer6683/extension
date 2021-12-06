033d9374e13d1cb820530786d0dfc4161639b8b1
Andrew Lou <ayl72@cornell.edu>
2021-12-06 14:53:29 -0500

    Pomodoro Form Creation

--- content.js
+++ content.js
@@ -1,15 +1,16 @@
-setInterval(
-  function () {
-    var date = new Date();
-    semi = "AM";
-    hour = date.getHours();
-    minute = date.getMinutes();
-    second = date.getSeconds();
-    if (hour > 11) { semi = "PM"; }
-    if (hour > 12) { hour = hour - 12; }
-    if (hour == 0) { hour = 12; }
-    if (minute < 10) { minute = "0" + minute; }
-    if (second < 10) { second = "0" + second; }
-    document.getElementById("clock").innerHTML = hour + ":" + minute + ":" + second + " " + semi;
-  }, 500)
-
+function clock() {
+  setInterval(
+    function () {
+      var date = new Date();
+      semi = "AM";
+      hour = date.getHours();
+      minute = date.getMinutes();
+      second = date.getSeconds();
+      if (hour > 11) { semi = "PM"; }
+      if (hour > 12) { hour = hour - 12; }
+      if (hour == 0) { hour = 12; }
+      if (minute < 10) { minute = "0" + minute; }
+      if (second < 10) { second = "0" + second; }
+      document.getElementById("clock").innerHTML = hour + ":" + minute + ":" + second + " " + semi;
+    }, 500);
+}
